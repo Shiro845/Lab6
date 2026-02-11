7af6f1ee770a32a83f7bbb5e5dd91da0b4c21396
Shiro845 <fvsandking@gmail.com>
2026-02-11 23:36:01 +0200

    add: brand & speed outputs

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,8 +1,8 @@
 ﻿List<Vehicle> vehicles = new List<Vehicle>
     {
-        new Car(),
-        new Bicycle(),
-        new Airplane()
+        new Car("Mercedes", 90),
+        new Bicycle("Canyon", 15),
+        new Airplane("Boeing", 900)
     };
 
 foreach (var vehicle in vehicles)
@@ -14,29 +14,38 @@ public abstract class Vehicle
 {
     protected string? Brand;
     protected int Speed;
+
+    public Vehicle(string brand,  int speed)
+    {
+        Brand = brand;
+        Speed = speed;
+    }
     public abstract void Move();
 }
 
 public class Car : Vehicle
 {
+    public Car(string brand, int speed) : base(brand, speed) { }
     public override void Move()
     {
-        Console.WriteLine("Автомобіль їздить");
+        Console.WriteLine($"Автомобіль марки {Brand} їде зі швидкістю {Speed} км/год");
     }
 }
 
 public class Bicycle : Vehicle
 {
+    public Bicycle(string brand, int speed) : base(brand, speed) { }
     public override void Move()
     {
-        Console.WriteLine("Велосипед їздить");
+        Console.WriteLine($"Велосипед марки {Brand} їде зі швидкістю {Speed} км/год");
     }
 }
 
 public class Airplane : Vehicle
 {
+    public Airplane(string brand, int speed) : base(brand, speed) { }
     public override void Move()
     {
-        Console.WriteLine("Літак летить");
+        Console.WriteLine($"Літак марки {Brand} летить зі швидкістю {Speed} км/год");
     }
 }
--- Lab6/obj/Debug/net10.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net10.0/Lab6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119b42177daf66c93af1f991cc0920b098f771a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
