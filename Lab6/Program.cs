119b42177daf66c93af1f991cc0920b098f771a3
Shiro845 <fvsandking@gmail.com>
2026-02-11 23:06:58 +0200

    first commit

---
+++ Lab6/Program.cs
@@ -0,0 +1,42 @@
+﻿List<Vehicle> vehicles = new List<Vehicle>
+    {
+        new Car(),
+        new Bicycle(),
+        new Airplane()
+    };
+
+foreach (var vehicle in vehicles)
+{
+    vehicle.Move();
+}
+
+public abstract class Vehicle
+{
+    protected string? Brand;
+    protected int Speed;
+    public abstract void Move();
+}
+
+public class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Автомобіль їздить");
+    }
+}
+
+public class Bicycle : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Велосипед їздить");
+    }
+}
+
+public class Airplane : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Літак летить");
+    }
+}
