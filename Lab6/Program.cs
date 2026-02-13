faaf9905a05c1cd96c5bea89b9b2bd35fceaceaa
Shiro845 <fvsandking@gmail.com>
2026-02-13 18:22:06 +0200

    Ex.3

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,84 +1,25 @@
-﻿namespace Lab6;
-public static class Program
-{
-    public static void Main()
-    {
-        List<UiWindow> windows = new List<UiWindow>
-        {
-            new MainWindow("Головне меню", 1000, 800),
-            new DialogWindow("Помилка підключення", 200, 100),
-            new ModalWindow("Налаштування", 1500, 1000)
-        };
+﻿using System;
+using System.Collections.Generic;
 
-        foreach (var window in windows)
+namespace LabPolymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Artifact> inventory = new List<Artifact>();
+
+        inventory.Add(new MagicScroll(101));
+        inventory.Add(new AncientSword(202));
+
+        Console.WriteLine("--- Аналіз інвентарю ---");
+
+        foreach (var item in inventory)
         {
-            window.Draw();
-            if (window is IResizable resizableWindow)
-            {
-                resizableWindow.Resize();
-            }
-            else
-            {
-                Console.WriteLine("Розмір вікна не може бути змінений користувачем");
-            }
+            item.Identify();
         }
-    }
-}
 
-public interface IResizable
-{
-    void Resize();
-}
-
-public abstract class UiWindow
-{
-    protected string? Title;
-    protected int Width;
-    protected int Height;
-
-    public UiWindow(string title, int width, int height)
-    {
-        Title = title;
-        Width = width;
-        Height = height;
-    }
-    public abstract void Draw();
-}
-
-public class MainWindow : UiWindow, IResizable
-{
-    public MainWindow(string title, int width, int height) : base(title, width, height) { }
-
-    public override void Draw()
-    {
-        Console.WriteLine($"MainWindow \"{Title}\" {Width}x{Height}, було відмальовано успішно");
-    }
-
-    public void Resize()
-    {
-        Console.WriteLine("Розмір вікна може бути змінений користувачем");
-    }
-}
-
-public class ModalWindow : UiWindow, IResizable
-{
-    public ModalWindow(string title, int width, int height) : base(title, width, height) { }
-
-    public override void Draw()
-    {
-        Console.WriteLine($"ModalWindow \"{Title}\" {Width}x{Height}, було відмальовано успішно");
-    }
-    public void Resize()
-    {
-        Console.WriteLine("Розмір вікна може бути змінений користувачем");
-    }
-}
-public class DialogWindow : UiWindow
-{
-    public DialogWindow(string title, int width, int height) : base(title, width, height) { }
-
-    public override void Draw()
-    {
-        Console.WriteLine($"DialogWindow \"{Title}\" {Width}x{Height}, було відмальовано успішно");
+        Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+        Console.ReadKey();
     }
 }
--- Lab6/obj/Debug/net10.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net10.0/Lab6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4dfb76d4a97b32b229c12cf72bf3749ff075d96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e00cf3b97bef88d1bad47a93df8d9d7a8240f6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
